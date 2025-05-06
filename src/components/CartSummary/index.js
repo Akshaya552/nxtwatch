22a65eae03a90bb7bf8ec60e5e34ee44b149449c
Akshaya552 <nxtwave@ip-10-2-137-217.ap-south-1.compute.internal>
2025-05-06 06:20:13 +0000

    first commit

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,29 +1,153 @@
+import {useState} from 'react'
+import Popup from 'reactjs-popup'
+import 'reactjs-popup/dist/index.css'
 import CartContext from '../../context/CartContext'
 import './index.css'
 
-const CartSummary = () => (
-  <CartContext.Consumer>
-    {value => {
-      const {cartList} = value
-      let total = 0
-      cartList.forEach(eachList => {
-        total += eachList.price * eachList.quantity
-      })
-      return (
-        <div className="cardsummary-container">
-          <div className="cardsummary-card">
-            <h1 className="total-text">
-              Order Total : <span className="amount">Rs {total}/-</span>
-            </h1>
-            <p className="items-text">{cartList.length} Items in cart</p>
-            <button type="button" className="checkout-button">
-              Checkout
-            </button>
+const CartSummary = () => {
+  const [iscod, setCod] = useState(true)
+  const [isConfirmed, setConfirmedStatus] = useState(false)
+  return (
+    <CartContext.Consumer>
+      {value => {
+        const {cartList} = value
+        let total = 0
+        cartList.forEach(eachList => {
+          total += eachList.price * eachList.quantity
+        })
+
+        const changeButtonStatus = () => {
+          setConfirmedStatus(true)
+        }
+
+        const oncodMarked = () => {
+          setCod(false)
+        }
+
+        return (
+          <div className="cardsummary-container">
+            <div className="cardsummary-card">
+              <h1 className="total-text">
+                Order Total : <span className="amount">Rs {total}/-</span>
+              </h1>
+              <p className="items-text">{cartList.length} Items in cart</p>
+
+              <Popup
+                modal
+                trigger={
+                  <button type="button" className="checkout-button">
+                    Checkout
+                  </button>
+                }
+              >
+                {close => (
+                  <div className="modal-container">
+                    {!isConfirmed && (
+                      <>
+                        <h1 className="total-text">
+                          Order Total :
+                          <span className="amount">Rs {total}/-</span>
+                        </h1>
+                        <p className="items-text">
+                          {cartList.length} Items in Cart
+                        </p>
+                        <p className="cart-product-title">
+                          select one of the payment methods :
+                        </p>
+                        <label
+                          className="disbaled-label"
+                          htmlFor="card"
+                          disabled
+                        >
+                          <input
+                            type="radio"
+                            disabled
+                            name="payment"
+                            id="card"
+                          />
+                          Card
+                        </label>
+                        <br />
+                        <label
+                          className="disbaled-label"
+                          htmlFor="net"
+                          disabled
+                        >
+                          <input
+                            type="radio"
+                            name="payment"
+                            id="net"
+                            disabled
+                          />
+                          Net Banking
+                        </label>
+                        <br />
+                        <label
+                          className="disbaled-label"
+                          htmlFor="upi"
+                          disabled
+                        >
+                          <input
+                            type="radio"
+                            name="payment"
+                            id="upi"
+                            disabled
+                          />
+                          UPI
+                        </label>
+                        <br />
+                        <label
+                          className="disbaled-label"
+                          htmlFor="wallet"
+                          disabled
+                        >
+                          <input
+                            type="radio"
+                            name="payment"
+                            id="wallet"
+                            disabled
+                          />
+                          Wallet
+                        </label>
+                        <br />
+                        <label htmlFor="cod" onClick={oncodMarked}>
+                          <input type="radio" name="payment" id="cod" /> Cash on
+                          Delivery
+                        </label>
+                        <br />
+                        <button
+                          type="button"
+                          className="checkout-button"
+                          disabled={iscod}
+                          onClick={changeButtonStatus}
+                        >
+                          Confirm Order
+                        </button>
+                      </>
+                    )}
+                    {isConfirmed && (
+                      <>
+                        <p className="cart-product-title">
+                          Your order has been placed successfully
+                        </p>
+                        <button
+                          type="button"
+                          className="checkout-button"
+                          onClick={() => close()}
+                        >
+                          Close
+                        </button>
+                      </>
+                    )}
+                  </div>
+                )}
+              </Popup>
+            </div>
           </div>
-        </div>
-      )
-    }}
-  </CartContext.Consumer>
-)
+        )
+      }}
+    </CartContext.Consumer>
+  )
+}
 
 export default CartSummary
